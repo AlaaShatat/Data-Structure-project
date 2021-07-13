870f4d60cfc4991f0e11ec6fd4bf3b65140ba7e7
Alaa-Ibrahim-Amer <69719564+Alaa-Ibrahim-Amer@users.noreply.github.com>
2021-07-13 04:55:51 +0200

    edit in formatting

--- XML_Checker_Proj/XML_Checker_Proj/XML.cs
+++ XML_Checker_Proj/XML_Checker_Proj/XML.cs
@@ -84,7 +84,7 @@ namespace xml_read
                 }
                
                     // closing tag 
-                }             
+                          
 
 
             
@@ -123,8 +123,9 @@ namespace xml_read
                     while (rdr.Peek() != '<')
                     {
                        tag_value += Convert.ToChar(rdr.Read());
+
                     }
-                    //Console.WriteLine(tag_value + " tag value  has been detected ");
+                
                     currentNode.TagValue = tag_value;
                 }
             }
@@ -136,51 +137,89 @@ namespace xml_read
        {
            // Format the XML and retun a string with XML formated.
            String result = "";
+        int levels = 0 ;
            for (int j = 0; j <  root_tags.Count; j++)
-           {    if (root_tags[j].attributes == null)
-                result +="<"+ root_tags[j].TagName +">"+Environment.NewLine;   
-                else 
-                result +="<"+ root_tags[j].TagName +" "+ root_tags[j].attributes +">"+Environment.NewLine;
+           {
+              result +="<"+ root_tags[j].TagName +" "+ root_tags[j].attributes +">"+Environment.NewLine;
+                levels = 0;
 
-                formating_childs(root_tags[j]);
-                 result +="</"+ root_tags[j].TagName +">"+Environment.NewLine;
+                formating_childs(root_tags[j]);  
+              
+                result +="</"+ root_tags[j].TagName + ">" + Environment.NewLine;
            }
        
-        void formating_childs(Tag mytag)
+        void formating_childs(Tag mytag )
         {
-             
+             for(int u =0 ;u < levels ; u++)
+                result +="      ";
+              if(levels != 0)
+                result +="<"+ mytag.TagName +" "+ mytag.attributes +">"+Environment.NewLine;
+          
                 if (mytag.Childs.Count != 0)
                 {
+                    levels ++ ;
+
                     for (int j = 0; j < mytag.Childs.Count; j++)
                     {
-                        result +="      ";
-                         if (mytag.attributes == null)
-                          result +="<"+ mytag.TagName + ">" + Environment.NewLine;
-                         else 
-                          result +="<"+ mytag.TagName +" "+ mytag.attributes +">"+Environment.NewLine;
                         
-                        formating_childs(mytag.Childs[j]);
-                       
-                        result +="      ";
-                        result +="</"+ mytag.TagName + ">" + Environment.NewLine;
+                        formating_childs(mytag.Childs[j] );
                     }
-              
+                   
+                     levels -- ;
+                    for(int u =0 ;u < levels ; u++)
+                         result +="      ";
+                  
+                  if(levels != 0)
+                     result +="</"+ mytag.TagName + ">" + Environment.NewLine;   
+                         
+                     
                 }
                   
-                if (mytag.TagValue != null)
-
-                {
-                      result +="      ";
-                         if (mytag.attributes == null)
-                          result +="<"+ mytag.TagName + ">" + Environment.NewLine;
-                         else 
-                          result +="<"+ mytag.TagName +" "+ mytag.attributes +">"+Environment.NewLine;
-                        
-                    result +="      "+ mytag.TagValue + Environment.NewLine;
-
-                      result +="      ";
-                        result +="</"+ mytag.TagName + ">" + Environment.NewLine;
-
+              else if (mytag.TagValue != null)
+               {
+                    string spaces = null ;
+                     for(int u =0 ;u < levels + 1 ; u++)
+                        { //result +="      ";
+                        spaces+="      ";
+                        }
+                      
+                        int position = 0 ; 
+                        string temp = null ;   
+                        string temp2 = null ;
+                        int y = 0 ;
+                        position = mytag.TagValue.IndexOf(Environment.NewLine);
+                           while(position != -1)
+                            {
+                               if (y==0)
+                            {
+                            position = mytag.TagValue.IndexOf(Environment.NewLine);
+                            temp = mytag.TagValue.Substring(position + 2);
+                            temp2 = mytag.TagValue.Substring(0,position); 
+                            } 
+                            if(temp == Environment.NewLine)
+                                 result += spaces +mytag.TagValue.Substring(0,position);
+                                 else
+                                 result += spaces + temp2+ Environment.NewLine;
+                                  
+                           position =  temp.IndexOf(Environment.NewLine);
+                           if (position != -1)
+                           {
+                            temp2 = temp.Substring(0,position); 
+                            temp = temp.Substring(position + 2);
+                           }
+                                  y = 1 ;
+                           }
+                    
+                          if (y==0)
+                         result += spaces + mytag.TagValue + Environment.NewLine;
+                  
+                   
+                    for(int u =0 ;u < levels ; u++)
+                         result +="      ";
+                  if(levels != 0)
+                     result +="</"+ mytag.TagName + ">" + Environment.NewLine; 
+                
+                       
                 }
 
 
@@ -188,78 +227,103 @@ namespace xml_read
         }
        
         
-        using (FileStream fs = File.Create(path))     
+        /*using (FileStream fs = File.Create(path))     
         {
                    byte[] info = new UTF8Encoding(true).GetBytes(result);
                     fs.Write(info, 0, info.Length);
         }
+        */
+
+      /*       int lineNumber = 1 ;
+         int columnNumber = 0 ;
+         FileStream _inputStream = new FileStream(this.path, FileMode.Open, FileAccess.Read);
+         using (StreamReader rdr = new StreamReader(_inputStream))
+        {            
+            int currentRead;
+            Tag currentParent = null;
+            Tag currentNode = null;
+            
+            while ((currentRead = rdr.Read()) >= 0)
+            {
+                char currentChar = Convert.ToChar(currentRead);
+                    result +=  currentChar ;
+                columnNumber++;
+                if (currentChar == '\n')
+                {
+                    lineNumber++;
+                    columnNumber = 0;
+                }
+                // Xml Version line
+                if ((currentChar == '<') && (rdr.Peek() == '?'))
+                {
+                             while (rdr.Peek() != '>')
+                    { 
+                        result += Convert.ToChar(rdr.Read());
+                        columnNumber++;
+                    }
+                                 result += Environment.NewLine;
+                }
+                // Tag Detection  
+                if (currentRead == '<' && rdr.Peek() != '/')
+                {
+                    
+                    while (rdr.Peek() != '>')
+                    { 
+                        result += Convert.ToChar(rdr.Read());
+                        columnNumber++;
+                    }
+
+                   result += Environment.NewLine;
+                }
+               
+                else if (currentRead == '<' && rdr.Peek() == '/') {
+                    rdr.Read();
+                    string tag_name = "";
+                    while (rdr.Peek() != '>')
+                    {
+                        tag_name += Convert.ToChar(rdr.Read());
+                        columnNumber++;
+                    }
+                    rdr.Read();
+                    if (tag_name != validationStack.Peek().TagName) {
+                        Console.WriteLine("Error: Tag not closed at Line "+ lineNumber + "Col." + columnNumber);
+                         errorLine = "Error: Tag not closed at Line "+ lineNumber + "Col." + columnNumber;
+                        //currentError.Text = errorLine;
+
+                    }
+                    Tag poped_node = validationStack.Pop();                 
+                    //Root Node if empty stack. 
+                    if (validationStack.Count == 0)
+                    {
+                        this.root_tags.Add(poped_node);
+                    }
+                    else {
+                        Tag lastOpenedNode = validationStack.Peek();
+                        lastOpenedNode.Childs.Add(poped_node);
+                    }
+                    // child to some parent 
+                    //Console.WriteLine(tag_name + " tag >  has been detected ");
+                }
+
+                else if (currentRead != '<' && currentRead != '\r' && currentRead != '\n')
+                {
+                    string tag_value = ""+Convert.ToChar(currentRead);
+                    while (rdr.Peek() != '<')
+                    {
+                       tag_value += Convert.ToChar(rdr.Read());
+
+                    }
+                
+                    currentNode.TagValue = tag_value;
+                }
+            }
+        }
+    }*/
            return result;
        }
 
        public string ConvertToJson() {
-           // Use list of root_tags to convert the xml to Json and return a string.
-           String result = "";
-
-           for (int j = 0; j <  root_tags.Count; j++)
-           {
-                if (root_tags[j].attributes == null)
-                result +=" \" " + root_tags[j].TagName + " \" "+Environment.NewLine;   
-                else 
-                result +="<"+ root_tags[j].TagName +" "+ root_tags[j].attributes +">"+Environment.NewLine;
-
-                formating_childs(root_tags[j]);
-                 result +="</"+ root_tags[j].TagName +">"+Environment.NewLine;
-           }
-       
-        void formating_childs(Tag mytag)
-        {
-             
-                if (mytag.Childs.Count != 0)
-                {
-                    for (int j = 0; j < mytag.Childs.Count; j++)
-                    {
-                        result +="      ";
-                         if (mytag.attributes == null)
-                          result +="<"+ mytag.TagName + ">" + Environment.NewLine;
-                         else 
-                          result +="<"+ mytag.TagName +" "+ mytag.attributes +">"+Environment.NewLine;
-                        
-                        formating_childs(mytag.Childs[j]);
-                       
-                        result +="      ";
-                        result +="</"+ mytag.TagName + ">" + Environment.NewLine;
-                    }
-              
-                }
-                  
-                if (mytag.TagValue != null)
-
-                {
-                      result +="      ";
-                         if (mytag.attributes == null)
-                          result +="<"+ mytag.TagName + ">" + Environment.NewLine;
-                         else 
-                          result +="<"+ mytag.TagName +" "+ mytag.attributes +">"+Environment.NewLine;
-                        
-                    result +="      "+ mytag.TagValue + Environment.NewLine;
-
-                      result +="      ";
-                        result +="</"+ mytag.TagName + ">" + Environment.NewLine;
-
-                }
-
-
-
-        }
-       
-        
-        using (FileStream fs = File.Create(path))     
-        {
-                   byte[] info = new UTF8Encoding(true).GetBytes(result);
-                    fs.Write(info, 0, info.Length);
-        }
-     
-            return result;
+         return "result";
      
        }
 
